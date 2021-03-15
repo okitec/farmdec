c7f9c4b7ca05c70e2ff41afbda899b9727e86717
Dominik Okwieka <dominik.okwieka@t-online.de>
2021-03-15 18:07:00 +0100

    main: add printinst, single-instruction mode
    Sometimes, you just want to run "farmdec <instr>". This is helpful
    for invocation in Awk, for example.

--- main.c
+++ main.c
@@ -12,8 +12,22 @@ enum {
 // char *mnemonics[], generated using mnemonics.awk.
 #include "mnemonics.h"
 
+void printinst(Inst inst);
+
 // main: read instructions as list of hexadecimal numbers, decode, print results.
 int main(int argc, char **argv) {
+	// Single instruction as argument?
+	if (argc == 2) {
+		u32 binst;
+		Inst inst;
+		sscanf(argv[1], "%x", &binst);
+		fad_decode(&binst, 1, &inst);
+		printinst(inst);
+		return 0;
+	}
+
+	// ----- Benchmark/Bulk mode -----
+
 	size_t capacity = 1;
 	u64 ninst = 0;
 	u32 *ibuf = calloc(capacity, sizeof(u32)); // dynamically resized
@@ -48,37 +62,40 @@ int main(int argc, char **argv) {
 	fad_decode(ibuf, ninst, obuf);
 
 	for (uint i = 0; i < ninst; i++) {
-		Inst inst = obuf[i];
-		char regch = (inst.flags & W32) ? 'W' : 'X';
-		char flagsch = (inst.flags & SET_FLAGS) ? 'S' : ' ';
-
-
-		switch (inst.op) {
-		case A64_UNKNOWN: {
-			u32 binst = inst.imm;
-
-			// Print unknown instruction as little-endian hex bytes. This format
-			// can be used by the Online Disassembler (ODA).
-			printf("%04x ??? %02x %02x %02x %02x\n", 4*i, (binst>>0) & 0xff, (binst>>8) & 0xff,
-				(binst>>16) & 0xff, (binst>>24) & 0xff);
-			continue;
-		}
-		case A64_ERROR:   printf("%04x error \"%s\"\n", 4*i, inst.error); continue;
-		case A64_UDF:     printf("%04x udf\n", 4*i);                      continue;
-		default:
-			break; // normal instruction
-		}
-
-		// We do not disambiguate here -- all instructions are printed
-		// the same; for example, instructions with two immediates have
-		// the imm field printed too.
-		printf("%04x %-12s %c %c%d, %c%d, %c%d, imm=%lu, offset=%+ld, fimm=%f, imm2=(%u,%u), flags=0b %o%o%o %o%o%o %o%o\n",
-			4*i, mnemonics[inst.op], flagsch,
-			regch, inst.rd, regch, inst.rn, regch, inst.rm,
-			inst.imm, inst.offset, inst.fimm, inst.bfm.lsb, inst.bfm.width,
-			(inst.flags>>7) & 1, (inst.flags>>6) & 1, (inst.flags>>5) & 1, (inst.flags>>4) & 1,
-			(inst.flags>>3) & 1, (inst.flags>>2) & 1, (inst.flags>>1) & 1, (inst.flags>>0) & 1);
+		printf("%06x ", 4*i); // addr in ibuf
+		printinst(obuf[i]);
 	}
 
 	return 0;
+}
+
+void printinst(Inst inst) {
+	char regch = (inst.flags & W32) ? 'W' : 'X';
+	char flagsch = (inst.flags & SET_FLAGS) ? 'S' : ' ';
+
+	switch (inst.op) {
+	case A64_UNKNOWN: {
+		u32 binst = inst.imm;
+
+		// Print unknown instruction as little-endian hex bytes. This format
+		// can be used by the Online Disassembler (ODA).
+		printf("??? %02x %02x %02x %02x\n", (binst>>0) & 0xff, (binst>>8) & 0xff,
+			(binst>>16) & 0xff, (binst>>24) & 0xff);
+		return;
+	}
+	case A64_ERROR:   printf("error \"%s\"\n", inst.error); return;
+	case A64_UDF:     printf("udf\n");                      return;
+	default:
+		break; // normal instruction
+	}
+
+	// We do not disambiguate here -- all instructions are printed
+	// the same; for example, instructions with two immediates have
+	// the imm field printed too.
+	printf("%-12s %c %c%d, %c%d, %c%d, imm=%lu, offset=%+ld, fimm=%f, imm2=(%u,%u), flags=0b %o%o%o %o%o%o %o%o\n",
+		mnemonics[inst.op], flagsch,
+		regch, inst.rd, regch, inst.rn, regch, inst.rm,
+		inst.imm, inst.offset, inst.fimm, inst.bfm.lsb, inst.bfm.width,
+		(inst.flags>>7) & 1, (inst.flags>>6) & 1, (inst.flags>>5) & 1, (inst.flags>>4) & 1,
+		(inst.flags>>3) & 1, (inst.flags>>2) & 1, (inst.flags>>1) & 1, (inst.flags>>0) & 1);
 }
